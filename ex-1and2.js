33828111cd767224e1c6822a8f2aee8b9cf5b811
aimwadee <aimwadee2010@gmail.com>
2024-08-16 14:00:58 +0700

    finished

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = Karter
+let Surname = Anderson
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurantName = "Chef's Table by Chef James"
+const netPrice = 15000
+const numberOpenRestaurant = 1500
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”
+netPrice = 5000
+numberOpenRestaurant = 2500
